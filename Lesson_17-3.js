9a29301e0c1f74b37dcd35fb0654393df11cb542
labyd <84662163+labyd@users.noreply.github.com>
2021-07-15 11:27:08 +0300

    Ввели условия

---
+++ Lesson_17-3.js
@@ -0,0 +1,22 @@
+var count=0;
+var key=false;
+if(key==true)
+{
+  document.write("Hellow world!");
+}
+
+else if (count==0){
+ document.write("xD")
+}
+
+else {
+  document.write("GoodBye");
+}
+
+switch (count) {
+  case 0:alert("Ноль");break;
+  case 1:alert("Один");break;
+  //case 3:alert("Два");break; так написал "учитель"
+  case 3:alert("Три");break; //это он исправил через 2-3 минуты. При этоп дважды прочитав и после третьего прочтения внес поправку
+  default:break;
+}
