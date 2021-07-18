9c93e1843932061f3d9900644cf62f5f603f1498
labyd <84662163+labyd@users.noreply.github.com>
2021-07-18 15:26:48 +0300

    Создали функции

---
+++ Lesson_17-6.js
@@ -0,0 +1,14 @@
+function Print(str)
+{
+  document.write(str);
+}
+
+function STriangl(a,h)
+{
+  return a*h*1/2;
+}
+
+function NewTriangle()
+{
+Print("<br/>Площадь треугольника равна: "+ STriangl(5,10))
+}
