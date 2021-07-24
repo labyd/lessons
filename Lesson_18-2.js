d663b9319e811a8050b23a908616c0237e9d5674
labyd <84662163+labyd@users.noreply.github.com>
2021-07-24 15:35:55 +0300

    Использовали селекторы css
    не работает

---
+++ Lesson_18-2.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('body').css('border','1px solid red');
+  $('.header').css('border', '1px solid blue');
+  $('#main_nav').css('border', '1px solid green');
+  $('ul>li').css('border', '1px solid black');
+});
