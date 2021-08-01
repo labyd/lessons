e17ae54c47080a2d60f442a0469262d9eccad25c
labyd <84662163+labyd@users.noreply.github.com>
2021-08-01 13:56:12 +0300

    JS3
    Урок 3

---
+++ Lesson_19-2.js
@@ -0,0 +1,53 @@
+let myLifeStyle = 'freelance';
+console.log(myLifeStyle);
+
+let myName = 'Фрилансер по жизни', myAge = 36, myMessage = 'Живи, а работай в свободное время!';
+
+let myNamE = 'Фрилансер по жизни',
+    myAgE = 36,
+    myMessagE = 'Живи, а работай в свободное время!';
+
+let myNamEs = 'Фрилансер по жизни';
+let myAgEs = 36;
+let myMessagEs = 'Живи, а работай в свободное время!';
+
+myAge = 18;
+console.log(myAge);
+
+let myNewAge = 18;
+myAge = myNewAge;
+console.log(myAge);
+
+let myA = 36;
+
+function testBlock() {
+  let myAge = 16;
+  console.log(myAge);
+  console.log('Учим');
+  console.log('JS');
+}
+testBlock();
+console.log(myA);
+
+const youreAge = 30;
+//youreAge = 18; НЕЛЬЗЯ ИЗМЕНЯТЬ КОНСТАНТУ
+
+const BLOCK_SIZE = 25;
+const COLOR_RED = "#F00";
+
+const summSizes = 25 + 30;
+
+const userProfile = {
+  name: 'Фрилансер',
+  age: 36,
+  message: 'Живи'
+}
+console.log(userProfile);
+
+userProfile.age = 18;
+console.log(userProfile);
+
+fila = 46;
+console.log(fila);
+
+var fila;
